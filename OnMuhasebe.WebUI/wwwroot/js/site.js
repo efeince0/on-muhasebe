cacc62996f6db5cfc4277ff6ffd96f44f7369f8d
efeince0 <efe.ince@ogr.sakarya.edu.tr>
2026-09-18 14:41:14 +0300

    Select prefilled input text on focus to prevent value concatenation

--- OnMuhasebe.WebUI/wwwroot/js/site.js
+++ OnMuhasebe.WebUI/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Onceden doldurulmus (oneri kod/no, varsayilan 0/0,00 gibi) metin alanlarina
+// tiklaninca/Tab ile gelininde tum icerik secili gelsin. Boylece kullanici
+// yazmaya basladiginda imlecin bulundugu yere karisip "C0008C0008" gibi
+// anlamsiz degerler olusmaz; uzerine yazmak yeterli olur. Tum inputlara
+// tek bir olay dinleyiciyle uygulaniyor, her forma ayri kod eklemeye gerek yok.
+document.addEventListener('focusin', function (e) {
+    var el = e.target;
+    if (el.tagName === 'INPUT' &&
+        (el.type === 'text' || el.type === 'number' || el.type === 'tel' || el.type === 'email')) {
+        el.select();
+    }
+});
